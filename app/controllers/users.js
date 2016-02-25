305c61e77062733e5b6ba4f50ffb188c8e3551ae
jean-luc <jean-luc@Ordiii.local>
2016-02-25 16:59:55 +0100

    Modification controller user

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
-  User = mongoose.model('User');
+  User = mongoose.model('User'),
+  Issue = mongoose.model('Issue');
 
 module.exports = function (app) {
   app.use('/api/users', router);
@@ -80,8 +81,12 @@ router.put('/:id', function (req, res, next) {
       return;
     }
 
-    user.name = req.body.name;
-    user.age = req.body.age;
+    user.forname = req.body.forname;
+    user.surname = req.body.surname;
+    user.nickname = req.body.nickname;
+    user.dob = req.body.dob;
+    user.email = req.body.email;
+    user.roles = req.body.date; //comme les roles peuvent être multiple, on a un tableau de roles.
 
     user.save(function(err, updatedUser) {
       if (err) {
@@ -112,6 +117,62 @@ router.delete('/:id', function (req, res, next) {
   });
 });
 
+
+// GET /api/users?sort=nbrIssues || Get the list of users who have created most issues.
+router.get('/api/users?sort=nbrIssues', function (req, res, next){
+  Issue.find(function (err, users) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+
+    res.send(users);
+  });
+});
+
+/* 
+ User.find(function (err, users) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+
+    res.send(users);
+  });
+  countIssues(filters, sorting, pagination)
+
+
+
+router.get('/:id', function (req, res, next) {
+
+  var nbrIssues = req.query.nbrIssues;
+
+  var criteria = {};
+  // Filter by publisher.
+  if (req.query.nbrIssues) {
+  criteria.publisherId = req.query.publisherId; }
+  // Filter by format. if (req.query.format) {
+  criteria.format = req.query.format; }
+  // Find all matching books. Book.find(criteria, function(err, books) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+  }
+    res.send(books);
+  });
+
+  User.findById(userId, function(err, user) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    } else if (!user) {
+      res.status(404).send('User not found');
+      return;
+    }
+
+    res.send(user);
+  });
+});
 // GET /api/users/:id
 /*
 router.get(req.query, function (req, res, next) {
