041e9c906f51542ba404645988796d186d48da23
jean-luc <jean-luc@Ordiii.local>
2016-02-25 10:28:24 +0100

    Ajout du controller users pour model User

---
+++ app/controllers/users.js
@@ -0,0 +1,106 @@
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
+
+module.exports = function (app) {
+  app.use('api/v1/users', router);
+};
+
+/**
+ * @api {post} /users Create a user
+ * @apiName CreateUser
+ * @apiGroup Users
+ *
+ * @apiSuccess {String} age Age of the user.
+ */
+router.post('/', function (req, res, next) {
+
+  var user = new User(req.body);
+
+  user.save(function (err, createdUser) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+
+    res.send(createdUser);
+  });
+});
+
+// GET /api/users
+router.get('/', function (req, res, next) {
+
+  User.find(function (err, users) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+
+    res.send(users);
+  });
+});
+
+// GET /api/users/:id
+router.get('/:id', function (req, res, next) {
+
+  var userId = req.params.id;
+
+  User.findById(userId, function(err, user) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    } else if (!user) {
+      res.status(404).send('User not found');
+      return;
+    }
+
+    res.send(user);
+  });
+});
+
+// PUT /api/users/:id
+router.put('/:id', function (req, res, next) {
+
+  var userId = req.params.id;
+
+  User.findById(userId, function(err, user) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    } else if (!user) {
+      res.status(404).send('User not found');
+      return;
+    }
+
+    user.name = req.body.name;
+    user.age = req.body.age;
+
+    user.save(function(err, updatedUser) {
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
+
+      res.send(updatedUser);
+    });
+  });
+});
+
+// DELETE /api/users/:id
+router.delete('/:id', function (req, res, next) {
+
+  var userId = req.params.id;
+
+  User.remove({
+    _id: userId
+  }, function(err, data) {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+
+    console.log('Deleted ' + data + ' documents');
+    res.sendStatus(204);
+  });
+});
