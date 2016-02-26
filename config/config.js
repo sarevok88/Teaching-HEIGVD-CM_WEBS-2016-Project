129e46bf698d5ff86d444c0f9352917e8bc70fd1
Val <bouda.88@gmail.com>
2016-02-26 09:13:26 +0100

    modif du config pour heroku

--- config/config.js
+++ config/config.js
@@ -26,8 +26,8 @@ var config = {
     app: {
       name: 'ioflux'
     },
-    port: 3000,
-    db: 'mongodb://localhost/ioflux-production'
+    port: process.env.PORT || 3000,
+    db: process.env.MONGOLAB_URI || 'mongodb://localhost/webs-2016-example-production'
   }
 };
 
